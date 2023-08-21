057ee4bc3786039e62d3ea9d71652c90bf8c5c51
TaiwoSoftware <davcodes24@gmail.com>
2023-08-21 07:53:53 +0100

    footer done

---
+++ app.js
@@ -0,0 +1,3 @@
+const footerParagraph = document.querySelector('#footer-paragraph');
+const theYear = new Date().getFullYear();
+footerParagraph.innerHTML = `&copy; Copyright COG ${theYear}. All Right Reserved. `;
